102005327ff1b5963101a7a6d259713775fc53a3
Andre Piantino <andre.piantino@tjsc.jus.br>
2024-03-05 17:24:12 -0300

    Created project cards

--- src/app/projetos/page.tsx
+++ src/app/projetos/page.tsx
@@ -1,11 +1,32 @@
+import Card from "@/components/card";
 import Title from "@/components/title";
 
-export default function About() {
+const openSources = [
+  { name: 'keycloak-protocol-cas', href: 'https://github.com/jacekkow/keycloak-protocol-cas', description: 'Melhoria nos Logs do plugin', image:"/img/projects/keycloak-protocol-cas.png" },
+  { name: 'qt-common', href: 'https://github.com/open-eid/qt-common', description: 'Tradução para portugês', image:"/img/projects/qt-common.png" },
+  { name: 'mkdocs', href: 'https://github.com/mkdocs/mkdocs', description: 'Tradução para portugês', image:"/img/projects/mkdocs.png" },
+]
+
+export default function Projects() {
   return (
-    <main>
-      <section>
+    <main className="space-y-8">
+      <section className="space-y-7">
         <Title>Projetos</Title>
-        <p>...</p>
+        <div className="grid grid-cols-3 gap-4">
+          <Card title="keycloak-db-user-federation" href="https://github.com/piantino/keycloak-db-user-federation" image="/img/projects/keycloak-db-user-federation.png" >
+            Plugin para keycloak que importa usuários para dentro da base local.
+          </Card>
+        </div>
+      </section>
+      <section className="space-y-7">
+        <Title>Contribuições em projetos Open Source</Title>
+        <div className="grid grid-cols-3 gap-4">
+          {openSources.map(oss =>
+            <Card key={oss.name} title={oss.name} href={oss.href} image={oss.image} className="flex-1">
+              {oss.description}
+            </Card>
+          )}
+        </div>
       </section>
     </main>
   );
---
+++ src/components/card.tsx
@@ -0,0 +1,26 @@
+import Image from "next/image";
+
+
+export default function Card({ children, title, href, image, className }: any) {
+    const cardImage = image ? image : "/next.svg"
+
+    console.info(cardImage)
+
+    return (
+        <div className={className + " bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700"}>
+            <a href={href}>
+                <div style={{ width: '100%', height: '200px', position: 'relative' }}>
+                    <Image src={cardImage} alt={title} fill={true} style={{ objectFit: 'fill' }} className="rounded-t-lg"></Image>
+                </div>
+            </a>
+            <div className="p-5">
+                <a href={href}>
+                    <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{title}</h5>
+                </a>
+                <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+                    {children}
+                </p>
+            </div>
+        </div>
+    )
+}
