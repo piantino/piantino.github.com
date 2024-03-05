3f88e0c08f786e7add75e648090c88daf36b50dc
Andre Piantino <andre.piantino@tjsc.jus.br>
2024-03-05 11:12:55 -0300

    Added other pages

---
+++ src/app/projetos/page.tsx
@@ -0,0 +1,10 @@
+export default function About() {
+  return (
+    <main>
+      <section>
+        <h1>Projetos</h1>
+        <p>...</p>
+      </section>
+    </main>
+  );
+}
---
+++ src/app/sobre/page.tsx
@@ -0,0 +1,10 @@
+export default function About() {
+  return (
+    <main>
+      <section>
+        <h1>Sobre</h1>
+        <p>...</p>
+      </section>
+    </main>
+  );
+}
