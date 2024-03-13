8caed04b8ac829149b60ee67701aeb2196295560
Andre Piantino <andre.piantino@tjsc.jus.br>
2024-03-13 13:15:11 -0300

    Added recognitions

--- src/app/reconhecimentos/page.tsx
+++ src/app/reconhecimentos/page.tsx
@@ -1,11 +1,19 @@
+import Card from "@/components/card";
 import Title from "@/components/title";
+import recognitions from "@/data/recognitions.json";
 
 export default function About() {
   return (
     <main>
       <section>
         <Title>Reconhecimentos</Title>
-        <p>...</p>
+        <div className="grid grid-cols-3 gap-4">
+          {recognitions.map(oss =>
+            <Card key={oss.name} title={oss.name} href={oss.href} image={oss.image}>
+              {oss.description}
+            </Card>
+          )}
+        </div>
       </section>
     </main>
   );
